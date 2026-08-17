45bd707393267802dcb50e3b9ef8d9c52c8d3728
foxihd <120084738+foxihd@users.noreply.github.com>
2026-08-18 00:02:23 +0700

    modify color `--ac` and `--mid` tone to pass WCAG AAA by default

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -215,14 +215,14 @@ function setColor() {
     {{ end }}
     const styles = {
         light: {
-            default: '--off: #000; --ac: {{ or $lite.ac "#36c" }}; --bg: {{ or $lite.bg "#f9f9f9" }}; --fg: {{ or $lite.fg "#111" }}; --mid:{{ or $lite.mid "gray" }};',
-            less: '--off: #000; --ac: {{ or $lite.less.ac $lite.ac "#36c" }}; --bg: {{ or $lite.less.bg "#e7e2e2" }}; --fg: {{ or $lite.less.fg "#13253d" }}; --mid:{{ or $lite.less.mid "#7d8490" }};',
-            more: '--off: #000; --ac: {{ or $lite.more.ac $lite.ac "#36c" }}; --bg: {{ or $lite.more.bg "#fff" }}; --fg: {{ or $lite.more.fg "#000" }}; --mid:{{ or $lite.more.mid "gray" }};'
+            default: '--off: #000; --ac: {{ or $lite.ac "#03b" }}; --bg: {{ or $lite.bg "#f9f9f9" }}; --fg: {{ or $lite.fg "#111" }}; --mid:{{ or $lite.mid "#545454" }};',
+            less: '--off: #000; --ac: {{ or $lite.less.ac $lite.ac "#03b" }}; --bg: {{ or $lite.less.bg "#e7e2e2" }}; --fg: {{ or $lite.less.fg "#13253d" }}; --mid:{{ or $lite.less.mid "#444850" }};',
+            more: '--off: #000; --ac: {{ or $lite.more.ac $lite.ac "#03b" }}; --bg: {{ or $lite.more.bg "#fff" }}; --fg: {{ or $lite.more.fg "#000" }}; --mid:{{ or $lite.more.mid "#595959" }};--border: 1pt solid var(--fg);'
         },
         dark: {
-            default: '--off: #fff; --ac: {{ or $dark.ac "#fa0" }}; --bg: {{ or $dark.bg "#111" }}; --fg: {{ or $dark.fg "#f9f9f9" }}; --mid:{{ or $dark.mid "gray" }};',
-            less: '--off: #fff; --ac: {{ or $dark.less.ac $dark.ac "#fa0" }}; --bg: {{ or $dark.less.bg "#13253d" }}; --fg: {{ or $dark.less.fg "#e7e2e2" }}; --mid:{{ or $dark.less.mid "#7d8490" }};',
-            more: '--off: #fff; --ac: {{ or $dark.more.ac $dark.ac "#fa0" }}; --bg: {{ or $dark.more.bg "#000" }}; --fg: {{ or $dark.more.fg "#fff" }}; --mid:{{ or $dark.more.mid "gray" }};'
+            default: '--off: #fff; --ac: {{ or $dark.ac "#fa0" }}; --bg: {{ or $dark.bg "#111" }}; --fg: {{ or $dark.fg "#f9f9f9" }}; --mid:{{ or $dark.mid "#9e9e9e" }};',
+            less: '--off: #fff; --ac: {{ or $dark.less.ac $dark.ac "#fa0" }}; --bg: {{ or $dark.less.bg "#13253d" }}; --fg: {{ or $dark.less.fg "#e7e2e2" }}; --mid:{{ or $dark.less.mid "#acafb9" }};',
+            more: '--off: #fff; --ac: {{ or $dark.more.ac $dark.ac "#fa0" }}; --bg: {{ or $dark.more.bg "#000" }}; --fg: {{ or $dark.more.fg "#fff" }}; --mid:{{ or $dark.more.mid "#969696" }};--border: 1pt solid var(--fg);'
         }
     };
     const fbg = getElements('#background-header, #background-footer');
